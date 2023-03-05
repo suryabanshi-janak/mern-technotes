ec425af9bdb2b2f61f3922a668706b883f8ed77f
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-03-06 00:49:38 +0545

    middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(logger);
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
 
@@ -16,6 +26,8 @@ app.all('*', (req, res) => {
     res.type('txt').send('404 Not Found');
   }
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3500;
 
