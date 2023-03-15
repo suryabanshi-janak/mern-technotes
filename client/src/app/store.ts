3e3171672c4d382131468fb97f0692d9654aa248
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-03-15 22:18:51 +0545

    auth slice setup

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { apiSlice } from './api/apiSlice';
+import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
---
+++ client/src/features/auth/authSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { accessToken } = action.payload;
+      state.token = accessToken;
+    },
+    logOut: (state, action: PayloadAction<void>) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logOut } = authSlice.actions;
+
+export default authSlice.reducer;
+
+export const selectCurrentToken = (state) => state.auth.token;
